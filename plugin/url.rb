af188f5bb3b4d2f520df65b96325bd094544fa13
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-01-08 20:02:36 +0900

    add url plugin

---
+++ plugin/url.rb
@@ -0,0 +1,39 @@
+# encoding: utf-8
+
+require 'mechanize'
+require 'open-uri'
+
+class Url < Kris::Plugin
+  def response(message)
+    channel = message.to
+    message = message.body
+
+    URI.extract(message, %w(http https)).map { |uri| page_title(uri) }.each do |title|
+      notice(channel, title)
+    end
+  rescue Exception => e
+    notice(@robot.channel, e.to_s)
+  end
+
+  private
+    def page_title(uri)
+      case uri
+      when /twitter.com/
+        get_tweet(uri)
+      else
+        Nokogiri::HTML(open(URI.escape(uri))).title
+      end
+    end
+
+    def get_tweet(uri)
+      body = Nokogiri::HTML(open(URI.escape(uri.gsub('#!/', ''))))
+      username = body.at('.js-action-profile-name b').text
+      tweet = body.at('.tweet-text').text.strip
+      datetime = body.at('.client-and-actions span.metadata span').text
+      "@#{username}: #{tweet} - #{datetime}"
+    rescue Mechanize::ResponseCodeError
+      "Sorry, that page doesn't exist! by Twitter.com"
+    rescue
+      'Undefined Error'
+    end
+end
---
+++ spec/url_spec.rb
@@ -0,0 +1,31 @@
+# encoding: utf-8
+
+require 'spec_helper'
+require 'url'
+
+describe Url do
+  before do
+    @url = Url.new
+  end
+
+  describe 'when access to the Google' do
+    it 'should get title' do
+      response = @url.response(FakeMessage.new('http://www.google.com'))
+      response.first.must_equal('Google')
+    end
+  end
+
+  describe 'when access to the Twitter' do
+    it 'should get tweet' do
+      response = @url.response(FakeMessage.new('https://twitter.com/#!/gongoZ/statuses/288559849971781633'))
+      response.first.must_match(/おなかすいたでげそ/)
+    end
+  end
+
+  describe 'when recieved message' do
+    it 'should not be accessed' do
+      response = @url.response(FakeMessage.new('Tomochan: test'))
+      response.size.must_equal(0)
+    end
+  end
+end
