35c0e08d0004e61be876329e4aa4c616dcbdd8d2
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-01-14 20:31:41 +0900

    use CGI unescape HTML

--- plugin/google_search.rb
+++ plugin/google_search.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-require 'uri'
+require 'cgi'
 require 'google-search'
 
 class GoogleSearch < Kris::Plugin
@@ -15,7 +15,7 @@ class GoogleSearch < Kris::Plugin
   private
     def search(keyword)
       Google::Search::Web.new(query: keyword, size: :small, language: :ja).map do |item|
-        "#{URI.unescape(item.title)} - #{item.uri}"
+        "#{CGI.unescapeHTML(item.title)} - #{item.uri}"
       end
     end
 end
