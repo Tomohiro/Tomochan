9f363fa9ce02d350fc166744a9d7b8740ebfb068
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-02-01 21:31:06 +0900

    add Glitch plugin

---
+++ plugin/glitch.rb
@@ -0,0 +1,22 @@
+# encoding: utf-8
+
+# https://gist.github.com/4347478
+# $ glitch Hey everyone check out her knee socks!
+# => Hey ͘everyo̼ne͎ ch͈eck out̪ ͑h̋er ͎k̉nͫe̍e s̷ocͧks!
+
+class Glitch < Kris::Plugin
+  GLITCH_TABLE = ("\u0300".."\u036f").to_a
+
+  def response(message)
+    if message.body =~ /^(glitch) (?<str>.+?)$/
+      notice(message.to, glitch($~[:str]))
+    end
+  end
+
+  private
+    def glitch(str)
+      str.each_char.map do |c|
+        c + rand(2).times.map { GLITCH_TABLE.sample }.join
+      end.join
+    end
+end
---
+++ spec/glitch_spec.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+require 'spec_helper'
+require 'glitch'
+
+describe Glitch do
+  before do
+    @glitch = Glitch.new
+  end
+
+  describe 'when word to glitch' do
+    it 'should get glitched words' do
+      response = @glitch.response(FakeMessage.new('glitch Hello, World!'))
+      response.last.wont_equal('Hello, World!')
+    end
+  end
+end
