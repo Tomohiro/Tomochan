8243f313502c387bc86b0b9c01c9f619902834cb
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-02-26 19:35:31 +0900

    fix test condition

--- spec/movie_showtime_spec.rb
+++ spec/movie_showtime_spec.rb
@@ -10,7 +10,7 @@ describe MovieShowtime do
 
   describe 'when search movie' do
     it 'should get showtimes' do
-      response = @showtime.on_privmsg(FakeMessage.new('劇場版の上映時間教えて'))
+      response = @showtime.on_privmsg(FakeMessage.new('字幕の上映時間教えて'))
       response.size.wont_equal(0)
     end
   end
