6e94155d55f9454bcd65dff764486ddc39e94094
Tomohiro TAIRA <tomohiro.t@gmail.com>
2014-08-30 19:23:23 +0900

    Remove bloken plugn

--- plugin/fortune.rb
+++ plugin/fortune.rb
@@ -4,25 +4,27 @@ require 'open-uri'
 require 'nokogiri'
 
 class Fortune < Kris::Plugin
-  def initialize(*args)
-    super
-    @sex = %w(
-      http://legacy.fortune.yahoo.co.jp/fortune/bin/omikuji?sex=m
-      http://legacy.fortune.yahoo.co.jp/fortune/bin/omikuji?sex=f
-    )
-  end
-
   def on_privmsg(message)
+    channel = message.to
+
     if message.body == 'おみくじ'
-      notice(message.to, oracle)
+      sex = [
+        'http://fortune.yahoo.co.jp/omikuji/result.html?sex=m',
+        'http://fortune.yahoo.co.jp/omikuji/result.html?sex=f'
+      ]
+
+      fortunes = {}
+      message  = ''
+
+      html = Nokogiri::HTML(open(sex.sample))
+      (html/'table/tr/td').each do |content|
+        fortunes[$1] = $2 if content.inner_text =~ /^今日のあなたの(.+?)は(.+?)。/
+      end
+
+      fortunes.reverse_each do |genre, fortune|
+        message << "#{genre}：#{fortune}　"
+      end
+      notice(channel, message)
     end
   end
-
-  private
-    def oracle
-      html = Nokogiri::HTML(open(@sex.sample))
-      html.search('td').map { |td|
-        td.text[/^今日のあなたの(.+?)は(.+?)。/] ?  "#{$1}: #{$2} " : nil
-      }.compact.uniq.join
-    end
 end
